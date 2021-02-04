5f00d248191649bbc45574da69f7ff63f4253165
Neobii <iwilldevelopcodeforyou@gmail.com>
2021-02-04 16:44:13 -0600

    added link destinations ☴

---
+++ pages/best-sh.js
@@ -0,0 +1,10 @@
+import Head from 'next/head'
+import Layout from '../components/layout'
+
+export default function bestSh () {
+  return (
+    <>
+      <h1> Best Sh</h1>
+    </>
+  )
+}
---
+++ pages/database.js
@@ -0,0 +1,10 @@
+import Head from 'next/head'
+import Layout from '../components/layout'
+
+export default function currentData () {
+  return (
+    <>
+      <h1> Current Data</h1>
+    </>
+  )
+}
