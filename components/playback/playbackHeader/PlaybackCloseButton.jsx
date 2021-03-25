bd40528416fb1be4e5594683281469e944f6f655
Neobii <iwilldevelopcodeforyou@gmail.com>
2021-03-24 21:50:23 -0500

    {+🍕}, <-- +☴

--- components/playback/playbackHeader/PlaybackCloseButton.jsx
+++ components/playback/playbackHeader/PlaybackCloseButton.jsx
@@ -1,9 +1,9 @@
 import Link from 'next/link'
 
 const PlaybackCloseButton = () => (
-  <div className="noDrag">
+  <div className="order-4 sm:order-3 noDrag">
     <Link href={"/"}>
-      <div className="order-4 sm:order-3 bg-gray-300 w-12 cursor-pointer text-center py-2 px-3 select-none">X</div>
+      <div className="bg-gray-300 w-12 cursor-pointer text-center py-2 px-3 select-none">X</div>
     </Link>
   </div>
 )
