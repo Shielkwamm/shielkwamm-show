67de639cd20e8f11e7755f538fbedb959e4979b2
Neobii <iwilldevelopcodeforyou@gmail.com>
2021-03-24 21:07:59 -0500

    draggable [+]△ --> +△△.. <-- _☴, ++☴

--- components/playback/Playback.tsx
+++ components/playback/Playback.tsx
@@ -4,15 +4,17 @@ import PlaybackActor from './playbackActor/PlaybackActor'
 import PlaybackHeader from './playbackHeader/PlaybackHeader'
 import PlaybackNavigation from './playbackNavigation/PlaybackNavigation'
 import PlaybackMessage from './playbackMessage/PlaybackMessage'
+import Draggable, {DraggableCore} from 'react-draggable'
 
 const Playback = ({ setup, state, isLast, query }) => (
-  <div className={classNames(styles.playbackWrapper)}>
+  <Draggable><div className={classNames(styles.playbackWrapper)}>
     <PlaybackHeader setup={setup} state={state} />
     <PlaybackActor setup={setup} state={state}/>
     <PlaybackMessage setup={setup} state={state} />
     <hr/>
     <PlaybackNavigation setup={setup} state={state} query={query} isLast={isLast}/>
   </div>
+  </Draggable>
 )
 
 export default Playback;
