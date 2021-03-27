3fc21bb4929b03e83cdfeedc4d2b29b7ccfa1a95
Neobii <iwilldevelopcodeforyou@gmail.com>
2021-03-27 00:09:46 -0500

    fixed z-index, added ship page {{ ☴ }}

--- components/navBar.tsx
+++ components/navBar.tsx
@@ -1,17 +1,18 @@
 const NavBar = () => {
+  const ship = "( { ship } )";
   return (
     <div>
-      <div className="border border-indigo-600 fixed bottom-0 left-0 w-400 h-full text-4xl text-center flex flex-col justify-between bg-pink-300 bg-opacity-75">
+      <div style={{zIndex: 667}} className="border border-indigo-600 fixed bottom-0 left-0 w-400 h-full text-4xl text-center flex flex-col justify-between bg-pink-300 bg-opacity-75">
         <div className="flex-none grid grid-col-1 space-y-2.5">
           <a href="https://guide.shielkwamm.com">⧊</a>
           <a href="https://acktueli.com">◫</a>
         </div>
-        <div className="flex-grow text-center text-2xl align-middle m-1"  style={{writingMode: "vertical-rl", textOrientation: "mixed"}}>
-          || ship ||
+        <div className="flex-grow text-center text-2xl align-middle m-1 select-none"  style={{writingMode: "vertical-rl", textOrientation: "mixed"}}>
+          <a href="/ship">{ship}</a>
         </div>
         <div className="flex-none grid grid-col-1 space-y-4">
-          <a href="https://shielkwamm.net">🌈</a>
           <a href="http://sh.shielkwamm.com">💎</a>
+          <a href="https://shielkwamm.net">🌈</a>
           <a href="https://github.com/orgs/Shielkwamm/projects">📷</a>
         </div>
         
---
+++ pages/ship.tsx
@@ -0,0 +1,19 @@
+import Head from 'next/head';
+const ship = "( { ship } )";
+
+const ShipPage = () => {
+  return (
+    <>
+    <Head>
+      <title>{ ship }</title>
+    </Head>
+    <h1>guide</h1>
+    <h1>acktueli</h1>
+    <h1>shell</h1>
+    <h1>shielkwammNet</h1>
+    <h1>managingExpectations</h1>
+    </>
+  )
+}
+
+export default ShipPage;
