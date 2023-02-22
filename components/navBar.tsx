a001d32aa9e95f7b012b86a1f66f8c4545eb9bdd
Adam Love <iwilldevelopcodeforyou@gmail.com>
2023-02-22 08:12:01 +0000

    changed colors and emojis 👆

--- components/navBar.tsx
+++ components/navBar.tsx
@@ -6,20 +6,20 @@ const NavBar = () => {
   const router = useRouter();
   return (
     <div>
-      <div style={{zIndex: 668}} className="fixed border border-indigo-600 bottom-0 right-0 w-14 h-full text-4xl text-center flex flex-col justify-between bg-pink-300 bg-opacity-75">
+      <div style={{zIndex: 668}} className="fixed border border-indigo-600 bottom-0 right-0 w-14 h-full text-4xl text-center flex flex-col justify-between bg-orange-300 bg-opacity-75">
         <div className="flex-none grid grid-col-1 space-y-2.5">
-          <a href="https://guide.shielkwamm.com">⧊</a>
+          <a href="https://guide.shielkwamm.com">(📕&#123;</a>
           <a href="https://shielkwamm.net">🌈</a>
-          <a href="https://sh.shielkwamm.com">🥚</a>
+          <a href="https://sh.shielkwamm.com">🔬</a>
           <a href="https://github.com/orgs/Shielkwamm/projects">📷</a>
         </div>
         <div className="flex-grow text-center text-2xl align-middle m-2 select-none"  style={{writingMode: "vertical-rl", textOrientation: "mixed"}}>
           {router.pathname === "/ship" ? <a href="/">{sh}</a>: <a href="/ship">{ship}</a>}
         </div>
         <div className="flex-none grid grid-col-1 space-y-4">
-          <a href="https://acktueli.com">◫</a>
+          <a href="https://acktueli.com">💻</a>
           <a href="https://u-n.herokuapp.com/organization/linkage/Shielkwamm">🎉</a>
-          <a href="https://github.com/InformationAsymmetry">&7B;👔&7D;</a>
+          <a href="https://github.com/InformationAsymmetry">&#123;👔&#125;</a>
         </div>
         
       </div>
