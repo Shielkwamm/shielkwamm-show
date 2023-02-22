4aff75a3134527b258eb5e6c6e4a4c1328a6710d
Adam Love <iwilldevelopcodeforyou@gmail.com>
2023-02-22 01:32:32 -0600

    updated 👍

--- components/ship/topNav.tsx
+++ components/ship/topNav.tsx
@@ -1,7 +1,9 @@
 const ShipTopNav = () => (
   <div style={{paddingRight: "8px"}} className="grid grid-col-1 space-y-2.5 text-right">
     <a href="https://guide.shielkwamm.com">guide</a>
-    <a href="https://acktueli.com">acktueli</a>
+    <a href="https://shielkwamm.net">shielkwammNet</a>
+    <a href="https://sh.shielkwamm.com">shell</a>
+    <a href="https://github.com/orgs/Shielkwamm/projects">projects</a>
   </div>
 )
 
