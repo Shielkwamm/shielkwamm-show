1e0ea8741a42ca1ee86acff7f301e5ab6e6b3891
Adam Love <iwilldevelopcodeforyou@gmail.com>
2023-02-22 01:31:01 -0600

    updated 👍

--- components/ship/bottomNav.tsx
+++ components/ship/bottomNav.tsx
@@ -1,9 +1,8 @@
 const ShipBottomNav = () => (
   <div style={{paddingRight: "8px"}} className="grid grid-col-1 space-y-4 text-right ">
-    <a href="http://sh.shielkwamm.com">shell</a>
-    <a href="https://shielkwamm.net">shielkwammNet</a>
-    <a href="https://github.com/orgs/Shielkwamm/projects">schedule</a>
-    <a href="https://github.com/orgs/Shielkwamm/projects">u-n</a>
+    <a href="https://acktueli.com">acktueli</a>
+    <a href="https://u-n.herokuapp.com/organization/linkage/Shielkwamm">u-n</a>
+    <a href="https://github.com/InformationAsymmetry">InformationAsymmetry</a>
   </div>
 )
 
