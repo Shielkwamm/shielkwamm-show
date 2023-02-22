4150ff4436c24b830b4d989610ac18b1ca545421
Adam Love <iwilldevelopcodeforyou@gmail.com>
2023-02-22 07:40:02 +0000

    updated 👍

--- components/navBar.tsx
+++ components/navBar.tsx
@@ -19,7 +19,7 @@ const NavBar = () => {
         <div className="flex-none grid grid-col-1 space-y-4">
           <a href="https://acktueli.com">◫</a>
           <a href="https://u-n.herokuapp.com/organization/linkage/Shielkwamm">🎉</a>
-          <a href="https://github.com/InformationAsymmetry">{👔}</a>
+          <a href="https://github.com/InformationAsymmetry">&7B;👔&7D;</a>
         </div>
         
       </div>
