d0d3187e3b36062b50343de2f52564f954521413
Adam Love <iwilldevelopcodeforyou@gmail.com>
2023-12-12 17:30:05 +0000

    changed shielkwamm.net emoji to 🔥, top emoji result  from 'amazon emoji' google search

--- components/navBar.tsx
+++ components/navBar.tsx
@@ -9,7 +9,7 @@ const NavBar = () => {
       <div style={{zIndex: 668}} className="fixed border border-indigo-600 bottom-0 right-0 w-14 h-full text-4xl text-center flex flex-col justify-between bg-orange-300 bg-opacity-75">
         <div className="flex-none grid grid-col-1 space-y-2.5">
           <a href="https://guide.shielkwamm.com">(📕&#123;</a>
-          <a href="https://shielkwamm.net">🌈</a>
+          <a href="https://shielkwamm.net">🔥</a>
           <a href="https://sh.shielkwamm.com">🔬</a>
           <a href="https://github.com/orgs/Shielkwamm/projects">📷</a>
         </div>
