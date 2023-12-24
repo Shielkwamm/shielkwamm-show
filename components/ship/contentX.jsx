e95c2578b11dc5e0ce90e4f7e8c8b832200378c4
Adam Love <iwilldevelopcodeforyou@gmail.com>
2023-12-24 11:39:43 +0000

    adding tiktok link *..|🧶 }

--- components/ship/contentX.jsx
+++ components/ship/contentX.jsx
@@ -2,6 +2,8 @@ import ShipContent from './content'
 
 const ShipContentX = () => (
   <div className="text-center text-2xl align-middle m-1">
+    <a style={{borderStyle: "groove", borderWidth: "5px", backgroundImage: "url(https://p16-oec-sg.ibyteimg.com/tos-alisg-i-aphluv4xwc-sg/f802b35f78ad48679bbf43dca9fc530a~tplv-aphluv4xwc-origin-image.image)",
+backgroundRepeat: "repeat", backgroundSize: "24"}} href="https://www.tiktok.com/@shielkwamm"><h3>cockpit</h3></a>
     <ShipContent />
   </div>
 )
